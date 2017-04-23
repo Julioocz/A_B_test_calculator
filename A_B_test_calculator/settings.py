22b0e6adc957b818b11fd3492bbe049e6023dc8f
Julio Navarro <julioocz@gmail.com>
2017-04-23 11:15:54 -0400

    Added main view for the calculator app.

--- A_B_test_calculator/settings.py
+++ A_B_test_calculator/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'A_B_test_calculator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Calculator app
+
+# Same as 95% of.
+MAX_SIGNIFICANCE_LEVEL = 0.05
--- calculator/views.py
+++ calculator/views.py
@@ -1,3 +1,44 @@
+from django.conf import settings
+from django.http import JsonResponse, HttpResponseBadRequest
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
+from . import ab_calculator
+
 
 # Create your views here.
+@csrf_exempt
+def index(request):
+    """
+    Index and main view of the A/B split test calculator app. It renders the calculator index on 
+    any GET request. It also calculates the significance and if the result is significant for 
+    POST request.
+    
+    POST request params:
+        visitors[control]: number of visitors for the control version of the experiment
+        conversions[control]: number of conversions for the control version of the experiment
+        visitors[variant]: number of visitors for the variant version for the experiment
+        conversions[variant]: number of conversions for the variant version of the experiment
+    """
+    if request.method == 'GET':
+        return render(request, 'calculator/index.html')
+
+    elif request.method == 'POST':
+        visitors_control = float(request.POST['visitors[control]'])
+        visitors_variant = float(request.POST['visitors[variant]'])
+        conversions_control = float(request.POST['conversions[control]'])
+        conversions_variant = float(request.POST['conversions[variant]'])
+
+        significance = ab_calculator.significance(visitors_control, conversions_control,
+                                                  visitors_variant, conversions_variant)
+
+        if significance < settings.MAX_SIGNIFICANCE_LEVEL:
+            significant = True
+
+        else:
+            significant = False
+
+        return JsonResponse({'significance': significance, 'significant': significant})
+
+    else:
+        return HttpResponseBadRequest()
