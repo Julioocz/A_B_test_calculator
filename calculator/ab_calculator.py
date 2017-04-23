66ccb8c6ef3efb031ca457ea1ecd5ad83efd42ff
Julio Navarro <julioocz@gmail.com>
2017-04-23 08:33:22 -0400

    Added statistical significance calculator and the correspondent test.

---
+++ calculator/ab_calculator.py
@@ -0,0 +1,48 @@
+"""
+    A/B split test calculator
+    ----------
+        
+    This module implements the statistical calculation of the A/B test split significance. The 
+    implementation was taken from https://www.periscopedata.com/blog/ab-testing-in-redshift.html
+    which contains the code to do the statistical analysis using the normal approximation method 
+    in redshift with scipy. This was translated to python using scipy too.
+"""
+
+from scipy.stats import norm
+
+
+def standard_error(sample_size, successes):
+    """
+    Calculates the standard error of a sample proportion.
+    
+    Formula: σp = sqrt [ p(1 - p) / n ]. 
+    with:
+    p = proportion of successes in sample (successes / sample size)    
+    
+    :param sample_size: the size of the sample 
+    :param successes: the number of successes on the given sample. 
+    :return: the standard error on the sample proportion -> σp
+    """
+    p = successes / sample_size
+    return (p * (1 - p) / sample_size) ** 0.5
+
+
+def significance(size_a, successes_a, size_b, successes_b):
+    """
+    Calculates the significance for an A/B test.
+    
+    :param size_a: Sample size of the experiment A
+    :param successes_a: Successes of the experiment A
+    :param size_b: Sample size fo the experiment B
+    :param successes_b: Successes of the experiment b
+    :return: The significance of the test.
+    """
+    p_a = successes_a / size_a
+    p_b = successes_b / size_b
+    se_a = standard_error(size_a, successes_a)
+    se_b = standard_error(size_b, successes_b)
+
+    numerator = (p_b - p_a)
+    denominator = (se_a ** 2 + se_b ** 2) ** 0.5
+
+    return norm.sf(abs(numerator / denominator))
--- calculator/tests.py
+++ calculator/tests.py
@@ -1,6 +1,31 @@
 from django.test import TestCase
 
+from . import ab_calculator
+
 
 # Create your tests here.
 class CalculatorTests(TestCase):
-    pass
+    def test_significance(self):
+        """
+        Tests the A/B split test significance for 2 cases:
+            case1:
+                - Visit numbers for the control version = 1000. Conversions = 100
+                - Visit numbers for the variant version = 1000. Conversions = 150
+
+                Expected result: 0.0383
+
+            case 2:
+                - Visit numbers for the control version = 500. Conversions = 30
+                - Visit numbers for the variant version = 500. Conversions = 38
+
+                Expected result: 0.1573
+            """
+
+        significance_case1 = ab_calculator.significance(size_a=1000, successes_a=100,
+                                                        size_b=1000, successes_b=125)
+
+        significance_case2 = ab_calculator.significance(size_a=500, successes_a=30,
+                                                        size_b=500, successes_b=38)
+        print(significance_case1, significance_case2)
+        self.assertTrue(significance_case1 - 0.0383 <= 0.001, True)
+        self.assertTrue(significance_case2 - 0.1573 <= 0.001, True)
